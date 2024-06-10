c8d6f006c1cdce47f9c07ae43680b723d1acfa62
harshkumargithu <165617988+harshkumargithu@users.noreply.github.com>
2024-06-10 17:01:08 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,45 @@
+
+
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+let videoBtn =document.querySelectorAll('.vid-btn');
+
+
+document.querySelector('.close-button').addEventListener("click",()=>{
+    loginForm.classList.remove('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
